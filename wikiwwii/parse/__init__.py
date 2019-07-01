865a17204785d4973078fb6d5f2f0ea8d03e947b
Philipp Kats <casyfill@gmail.com>
2019-07-01 10:00:26 -0400

    black

--- wikiwwii/parse/__init__.py
+++ wikiwwii/parse/__init__.py
@@ -1,4 +1,9 @@
 from .bellengerets import _swap_ensure_sides
 from .casualties import _parse_casualties
 from .dates import extract_dates
-from .geocode import nominatim_geocode, extract_latlon, geocode_location, process_locations
+from .geocode import (
+    nominatim_geocode,
+    extract_latlon,
+    geocode_location,
+    process_locations,
+)
--- wikiwwii/parse/casualties.py
+++ wikiwwii/parse/casualties.py
@@ -1,39 +1,43 @@
 import pandas as pd
 
-DIGIT_PATTERN = '([\d|\,|\.]+)(?:\[\d+\])?\+?\s*(?:{words})'
-ONLY_DIGITS = '([\d|\,|\.]+)\Z'
-KEYWORDS = { 'killed': ['men', 'dead', 'killed', 'casualties', 'kia'], 
-             'wounded': ['wounded', 'sick', 'injured'], 
-             'captured': ['captured', 'prisoners'],
-             'tanks': ['tank', 'panzer'],
-             'airplane': ['airplane', 'aircraft'],
-             'guns': ['artillery', 'gun', 'self propelled guns', 'field-guns', 'anti-tank guns'],
-             'ships': ['warships', 'boats', 'destroyer', 'minelayer'],
-             'submarines': ['submarines']
+DIGIT_PATTERN = "([\d|\,|\.]+)(?:\[\d+\])?\+?\s*(?:{words})"
+ONLY_DIGITS = "([\d|\,|\.]+)\Z"
+KEYWORDS = {
+    "killed": ["men", "dead", "killed", "casualties", "kia"],
+    "wounded": ["wounded", "sick", "injured"],
+    "captured": ["captured", "prisoners"],
+    "tanks": ["tank", "panzer"],
+    "airplane": ["airplane", "aircraft"],
+    "guns": ["artillery", "gun", "self propelled guns", "field-guns", "anti-tank guns"],
+    "ships": ["warships", "boats", "destroyer", "minelayer"],
+    "submarines": ["submarines"],
 }
 
+
 def _shy_convert_numeric(v):
-    if pd.isnull(v) or v in (',', '.'):
+    if pd.isnull(v) or v in (",", "."):
         return pd.np.nan
-    
-    return int(v.replace(',', '').replace('.', ''))
-    
+
+    return int(v.replace(",", "").replace(".", ""))
+
 
 def _parse_casualties(column, keywords=KEYWORDS):
     df = pd.DataFrame(index=column.index, columns=KEYWORDS.keys())
 
-    
     for tp, keys in keywords.items():
         pattern = DIGIT_PATTERN.format(words="|".join(keys))
         extracted = column.str.lower().str.extractall(pattern).unstack()
         df[tp] = extracted.applymap(_shy_convert_numeric).min(1)
-        
+
     df = df.fillna(0).astype(int)
-    
+
     # only digits
-    b = column.fillna('').str.extract(ONLY_DIGITS).applymap(_shy_convert_numeric).iloc[:, 0]
+    b = (
+        column.fillna("")
+        .str.extract(ONLY_DIGITS)
+        .applymap(_shy_convert_numeric)
+        .iloc[:, 0]
+    )
     mask = b.notnull()
-    df.loc[mask, 'killed'] = b[mask]
+    df.loc[mask, "killed"] = b[mask]
     return df
-
-
